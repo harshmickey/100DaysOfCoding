02113199d821818fa5bb88f0b47a945ed4f55c2d
harshmickey <harshmickey30@gmail.com>
2023-10-09 21:32:32 +0530

    Create MatrixAddition.java
    Question : You are given two matrices A & B of same size, you have to return another matrix which is the sum of A and B.
    Note: Matrices are of same size means the number of rows and number of columns of both matrices are equal.

---
+++ Day 6/MatrixAddition.java
@@ -0,0 +1,29 @@
+package com.array.assignment;
+
+public class MatrixAddition {
+	
+	public int[] solve(int[][] A) {
+        int[] arr=new int[A[0].length];
+		int sum=0;
+		int count=0;
+		for(int j=0;j<A[0].length;j++)
+		{
+			for(int i=0;i<A.length;i++) {
+
+				sum=sum+A[i][j];
+			
+				
+			}	arr[count]=sum;
+				count++;
+                sum=0;
+		}return arr;
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+
+	}
+
+}
